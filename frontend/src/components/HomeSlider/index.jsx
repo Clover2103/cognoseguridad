d79adc07c3f00bcb1a5d96dbb3db344fe6727dd1
Cristian Rubio <cristianfrubio01@gmail.com>
2025-10-18 10:23:56 -0500

    Se realiza modificacion del componente de slider de la pagina

--- frontend/src/components/HomeSlider/index.jsx
+++ frontend/src/components/HomeSlider/index.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import imagen1 from "../../assets/home/slider/imagen1.jpeg";
+import imagen1 from "../../assets/home/slider/imagen1.jpg";
 import imagen2 from "../../assets/home/slider/imagen2.jpg";
 import imagen3 from "../../assets/home/slider/imagen3.jpg";
 import imagen4 from "../../assets/home/slider/imagen4.jpg";
 import imagen5 from "../../assets/home/slider/imagen5.jpg";
-import imagen6 from "../../assets/home/slider/imagen6.jpg";
-import imagen7 from "../../assets/home/slider/imagen7.jpg";
-import imagen8 from "../../assets/home/slider/imagen8.jpg";
-import imagen9 from "../../assets/home/slider/imagen9.jpg";
 import { useCarrucel } from "../../hooks/useCarrucel";
 import './HomeSlider.css';
 
-const imagenes = [imagen1, imagen2, imagen3, imagen4, imagen5, imagen6, imagen7, imagen8, imagen9];
+const imagenes = [imagen1, imagen2, imagen3, imagen4, imagen5];
 
 const HomeSlider = () => {
   const { 
@@ -29,32 +25,8 @@ const HomeSlider = () => {
           {/* Aquí podrías agregar contenido adicional si es necesario */}
         </div>
         <div className="rightHomeSlider col-md">
-          <div className="descriptionHomeSlider">
-            <h3>¡BIENVENIDOS!</h3>
-            <p>"A veces tienes que pensar en algo más que en tu propia seguridad, a veces tienes que pensar en el bien mayor"</p>
-            <p>Joanne Kathelen Rowling</p>
-            <Link to="/curses">
-              <button>
-                CONOCE NUESTROS CURSOS
-              </button>
-            </Link>
-          </div>
         </div>
       </div>
-
-      <div className="circuleHomeSlider1"></div>
-      <div className="circuleHomeSlider2"></div>
-      <div className="circuleHomeSlider3"></div>
-      <div className="circuleHomeSlider4"></div>
-      <div className="circuleHomeSlider5"></div>
-      <div className="circuleHomeSlider6"></div>
-      <div className="circuleHomeSlider7"></div>
-      <div className="circuleHomeSlider8"></div>
-      <div className="circuleHomeSlider9"></div>
-      <div className="circuleHomeSlider10"></div>
-      <div className="circuleHomeSlider11"></div>
-      <div className="circuleHomeSlider12"></div>
-      <div className="circuleHomeSlider13"></div>
     </div>
   );
 };
